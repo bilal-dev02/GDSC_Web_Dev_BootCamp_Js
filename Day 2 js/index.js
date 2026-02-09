55f4f8020242970436df9c6f1f5ca3a3f697a91b
Bilal <bilaldev169@gmail.com>
2026-02-09 22:25:52 +0500

    Day 2 js

---
+++ Day 2 js/index.js
@@ -0,0 +1,169 @@
+
+"use strict";
+
+const input = require("prompt-sync")();
+// let name = "Zayam"
+// let age = 20;
+// console.log("My name is " + name + " and my age is " + age)
+// console.log(`My name is ${name} and my age is ${age} `);
+
+// objects
+
+// /let name = "Zayam"
+// let age = 20;
+// let adress = "Dhamtoor"
+
+// const person = {
+//     // key    value 
+//    name :"Zayam",
+//    age:20 ,
+//    adress:"Dhamtor"
+// }
+
+// console.log(person);
+// console.log(person.name);
+// console.log(person["age"]);
+
+// person.name="Sahil"
+// person.age = 19;
+// person.area = "Abbottabad"
+
+// delete person.adress;
+
+// console.log(person);
+
+// for(let key in person){
+//     console.log( key + ":" +  person[key]);
+    
+// }
+
+
+// person = "Ali"
+// console.log(person);
+
+// const book = {
+//     // key    value 
+//     book1:"Phy",
+//     book2:"Maths",
+//     urdu :{
+//         urdu1:"Urdu A",
+//         urdu2:"Urdu B",
+//     },
+
+//      english :{
+//         english1:"English A",
+//         english2:"ENglish B",
+//     }
+
+// }
+
+// console.log(book.urdu.urdu1);
+
+// const obj = {
+//     name:"Name",
+//     age :19,
+//     pass:true,
+//     arr:[],
+//     greet: function (){
+//         console.log("Hello");
+        
+//     }
+
+// }
+
+// obj.greet();
+
+
+
+// const book = {
+//     // key    value 
+//     book1:"Phy",
+//     book2:"Maths",
+    
+//     urdu:{
+//         val : "val1",
+//         val2 :"val2",
+//     },
+
+//       eng:{
+//         val : "val1",
+//         val2 :"val2",
+//     }
+  
+//     }
+
+
+    // for(let key in book.urdu){
+    //     console.log(book.urdu[key]);
+        
+    // }
+
+
+//    console.log(this);
+
+//    const obj = {
+//        name :"Ali",
+//        fun : function (){
+//         console.log(this.name);
+        
+//        }
+//    }
+   
+    
+//     const a = obj.fun.bind(obj)
+//     a();
+
+//  let name = input("Enter your name : ")
+//  console.log(name);
+
+
+// function functionname(){
+
+// }
+
+//    const obj = {
+//        name :"Ali",
+//        fun : function (){
+          
+//            function fn(){
+//               console.log(this);
+              
+//           }
+
+//           const newfn=()=>{
+//                   console.log(this);
+//           }
+        
+//           newfn()
+         
+        
+//        }
+//    }
+
+//    obj.fun()
+ 
+
+
+
+
+
+
+
+
+     
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
