d8cf8a21ecf7acaca445e75c2fb378791c402b0f
Bilal <bilaldev169@gmail.com>
2026-03-06 23:15:42 +0500

    Day 10 js

---
+++ Day 10 js/index.js
@@ -0,0 +1,70 @@
+// JSON   javascript object notation   light weight 
+
+// const obj = {
+//     name :"Ali",
+//     age : 19
+// }  // heavy 
+// console.log(obj.name);
+
+// const json = {
+//     "name":'Sahil',
+//     "age":  20,
+// }
+// const message = {
+//     content:"HI",
+//     time : 1
+// }
+
+// object - stringify - object 
+
+// {
+//     name : "ALi",
+//     age : 19 ,
+// }  
+//      frontend 
+//      | 
+// {
+//      "name" : "ALi",
+//     "age" : 19 ,
+// }
+//     | 
+//     Backend 
+//     {
+//     name : "ALi",
+//     age : 19 ,
+// }
+//     | 
+//     {
+//     "name" : "ALi",
+//     "age" : 19 ,
+// }
+//   {
+//     name : "ALi",
+//     age : 19 ,
+// }
+
+
+
+
+
+// const data = JSON.stringify(message); 
+
+// console.log(data);
+
+// const newobj = JSON.parse(data)
+// console.log(newobj.time);
+
+
+
+// localStorage  
+
+
+
+
+
+
+
+
+
+
+
